f1fb08e3017ef603ae567493040730e85cdb5017
Chris Bongers <chrisbongers@gmail.com>
2022-01-14 15:55:59 +0200

    feat: Unfinished: Bookmark invalidation

--- packages/shared/src/hooks/feed/useFeedBookmarkPost.ts
+++ packages/shared/src/hooks/feed/useFeedBookmarkPost.ts
@@ -5,6 +5,7 @@ import { postAnalyticsEvent, optimisticPostUpdateInFeed } from '../../lib/feed';
 import { Post } from '../../graphql/posts';
 import AuthContext from '../../contexts/AuthContext';
 import AnalyticsContext from '../../contexts/AnalyticsContext';
+import { QueryClient, useQueryClient } from 'react-query';
 
 export default function useFeedBookmarkPost(
   items: FeedItem[],
@@ -20,14 +21,44 @@ export default function useFeedBookmarkPost(
 ) => Promise<void> {
   const { user, showLogin } = useContext(AuthContext);
   const { trackEvent } = useContext(AnalyticsContext);
+  const queryClient = useQueryClient();
+
+  const addBookmark = async (updatePost) => {
+    const queryKey = ['bookmarks', user?.id];
+
+    const { pages } = await queryClient.getQueryData(queryKey);
+
+    const index = pages[0]?.page?.edges.findIndex(
+      ({ node }) => node.id === updatePost.id,
+    );
+    if (index === -1) {
+      pages[0].page?.edges.push({ node: updatePost });
+    }
+    console.log(pages, index);
+  };
+
+  const deleteBookmark = async (post) => {
+    const queryKey = ['bookmarks', user?.id];
+    await queryClient.cancelQueries(queryKey);
+    const { pages, ...rest } = await queryClient.getQueryData(queryKey);
+
+    const items = pages[0]?.page?.edges.filter(
+      ({ node }) => node.id !== post.id,
+    );
+
+    pages[0].page.edges = items;
+    queryClient.setQueryData(queryKey, { rest, pages });
+  };
 
   const { bookmark, removeBookmark } = useBookmarkPost<{
     id: string;
     index: number;
   }>({
-    onBookmarkMutate: optimisticPostUpdateInFeed(items, updatePost, () => ({
-      bookmarked: true,
-    })),
+    onBookmarkMutate: optimisticPostUpdateInFeed(items, updatePost, () => {
+      return {
+        bookmarked: true,
+      };
+    }),
     onRemoveBookmarkMutate: optimisticPostUpdateInFeed(
       items,
       updatePost,
@@ -52,9 +83,12 @@ export default function useFeedBookmarkPost(
         },
       ),
     );
+
     if (bookmarked) {
+      addBookmark(post);
       await bookmark({ id: post.id, index });
     } else {
+      deleteBookmark(post);
       await removeBookmark({ id: post.id, index });
     }
   };
